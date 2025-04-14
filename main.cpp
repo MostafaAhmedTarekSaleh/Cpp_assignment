9cc55ec77711ec5531f78e849d243cbeb32127a9
Hasti-fgh <hstifq@gmail.com>
2025-04-14 12:18:46 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -29,6 +29,37 @@ public:
        difficulty++;   
    }
 };
+
+// FlashCardDeck Class: Manage and store flash cards
+class FlashCardsDeck{
+private:
+    vector<FlashCard> cards;
+
+public://add new cards to the deck
+    void addCard(string q, string a){
+        cards.push_back(FlashCard(q, a));
+    }    
+    
+    vector<FlashCard> &getCards(){
+        return cards;
+    }//list of flash cards
+//track user's score
+class User{
+public:
+    string name;
+    int score;
+    
+    User(string n){
+        name = n;
+        score = 0;
+    }
+
+    void ShowScore(){
+        cout<< name << "' score: "<<score << endl;
+    }
+}    
+
+};
 int main()
 {
     cout << "Hello world!" << endl;

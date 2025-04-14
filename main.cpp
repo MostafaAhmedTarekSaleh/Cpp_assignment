b6b106c35c1b8f59949de0de19359912a19a4135
Hasti-fgh <hstifq@gmail.com>
2025-04-14 11:32:16 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
-
 using namespace std;
+//class flash cards
+class FlashCard{
+public:
+   string questions;
+   string answer;
+   int difficulty;
 
+   FlashCard(string q, string a){
+       questions = q;
+       answer = a;
+       difficulty = 1;
+   }
+
+   void FlashCard::show(){
+    cout << "Q: "<< questions << endl;
+   }
+
+   bool FlashCard::checkAns(string userAns) {
+    return userAns == answer;
+   }
+
+
+   void updateDiff(bool correct){
+    if (correct && difficulty > 1)
+       difficulty--;
+    else if (! correct && difficulty < 5)
+       difficulty++;   
+   }
+};
 int main()
 {
     cout << "Hello world!" << endl;
